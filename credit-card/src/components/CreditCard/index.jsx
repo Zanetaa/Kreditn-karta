fd7067171e6b8c6cbc7b82ee73f4222f13a3c709
Zanetaa <91408269+Zanetaa@users.noreply.github.com>
2023-11-18 14:19:59 +0100

    Updated

--- credit-card/src/components/CreditCard/index.jsx
+++ credit-card/src/components/CreditCard/index.jsx
@@ -1,11 +1,33 @@
 import { useState, useEffect, useRef } from 'react';
+import './style.css';
 
 const CreditCard = () => {
   const [cardInputVisible, setCardIinputVisible] = useState(false);
 
-  const cardInputRef = useRef();
-
   const handleCredit = () => setCardIinputVisible(true);
+
+  const cardInputRef = useRef();
+  const cardSecondRef = useRef();
+  const cardThirdRef = useRef();
+  const cardFourthRef = useRef();
+
+  const handleInput = (event) => {
+    if (event.target.value.length == 4) {
+      cardSecondRef.current.focus();
+    }
+  };
+
+  const handleSecond = (event) => {
+    if (event.target.value.length == 4) {
+      cardThirdRef.current.focus();
+    }
+  };
+
+  const handleThird = (event) => {
+    if (event.target.value.length == 4) {
+      cardFourthRef.current.focus();
+    }
+  };
 
   useEffect(() => {
     if (cardInputVisible) {
@@ -16,8 +38,48 @@ const CreditCard = () => {
   return (
     <div className="kreditka">
       <h2>Zadejte číslo své kreditní karty</h2>
-      <button onClick={handleCredit}>Zadat číslo</button>
-      {cardInputVisible ? <input type="text" ref={cardInputRef} /> : null}
+      <button className="button" onClick={handleCredit}>
+        Zadat číslo
+      </button>
+      {cardInputVisible ? (
+        <input
+          key="1"
+          type="text"
+          minLength="4"
+          maxLength="4"
+          ref={cardInputRef}
+          onChange={handleInput}
+        />
+      ) : null}
+      {cardInputVisible ? (
+        <input
+          key="2"
+          type="text"
+          minLength="4"
+          maxLength="4"
+          ref={cardSecondRef}
+          onChange={handleSecond}
+        />
+      ) : null}
+      {cardInputVisible ? (
+        <input
+          key="3"
+          type="text"
+          minLength="4"
+          maxLength="4"
+          ref={cardThirdRef}
+          onChange={handleThird}
+        />
+      ) : null}
+      {cardInputVisible ? (
+        <input
+          key="4"
+          type="text"
+          minLength="4"
+          maxLength="4"
+          ref={cardFourthRef}
+        />
+      ) : null}
     </div>
   );
 };
